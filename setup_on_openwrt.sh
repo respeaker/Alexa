51ab197ffca0a7825a5bbe202d9f8547b22feb6f
Yihui Xiong <xiongyihui3@gmail.com>
2016-09-02 17:27:15 +0800

    manually merged code from Don

--- setup_on_openwrt.sh
+++ setup_on_openwrt.sh
@@ -1,4 +1,9 @@
 
+CURRENT_DIR="$( pwd )"
+SCRIPT_DIR="$( cd "$( dirname "$[0]" )" && pwd )"
+
+cd $SCRIPT_DIR
+
 if [ ! -d respeaker ]; then
     echo 'not found respeaker python library, get it from https://github.com/respeaker/respeaker_python_library'
     git clone https://github.com/respeaker/respeaker_python_library.git
@@ -8,7 +13,4 @@ if [ ! -d respeaker ]; then
     pip install CherryPy requests
 fi
 
-
-
-
-
+cd $CURRENT_DIR
